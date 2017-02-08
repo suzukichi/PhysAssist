11e3bd6065e408daa0782cd341d37f638f33cf6c
Daniel Sabsay <dsabsay@calpoly.edu>
2017-02-08 14:41:32 -0800

    Task #634931 - Make class skeletons for User and Role.

---
+++ Role.java
@@ -0,0 +1,12 @@
+/*
+ * Role.java
+ * 
+ * Abstract class for user roles.
+ * 
+ * @author Daniel Sabsay
+ * Created Feb 8, 2017
+ */
+
+public abstract class Role {
+  public abstract void displayPage(Page page);
+}
---
+++ User.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * User.java
+ * 
+ * Holds all information related to a user.
+ * 
+ * @author Daniel Sabsay
+ * Created Feb 8, 2017
+ */
+
+public class User {
+  private String username;
+  private String firstName;
+  private String lastName;
+  private long userID;
+  private String password;
+  private String email;
+  private long registrationDate;
+  private List<Classroom> classroomsEnrolled;
+  private List<Classroom> classroomsOwned;
+  
+  public User(String username, String firstName, String lastName, String password, String email) {
+    this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+    this.email = email;
+    
+    this.classroomsEnrolled = new ArrayList<Classroom>();
+    this.classroomsOwned = new ArrayList<Classroom>();
+  }
+  
+  /*
+   * Enroll the user in a classroom.
+   */
+  public void enrollClassroom(long classroom) {
+    
+  }
+  
+  /*
+   * Drop the user from a classroom.
+   */
+  public void dropClassroom(long classroom) {
+    
+  }
+  
+  // duplicate of dropClassroom() ?
+  public void removeClassroom(long classroom) {
+    
+  }
+  
+  /*
+   * Returns the user's permissions for the specified page.
+   */
+  public Role getPermission(Page page) {
+    
+  }
+}
