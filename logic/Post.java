c97e39d61410de8d5e732f22f80c98f34e584f5b
suzukichi <dmchiu@calpoly.edu>
2017-02-26 23:31:18 -0800

    Posts: fill in with db functions
    Now that the DB class is mostly set up, lets try to use it for reals.

--- logic/Post.java
+++ logic/Post.java
@@ -1,4 +1,7 @@
 package logic;
+
+import java.util.HashMap;
+
 /**
  * Post represents notifications posted by a classrooms teacher or contributor.
  *
@@ -17,13 +20,48 @@ public class Post {
     	this.text = text;
     }
     
-    public Post(int postID) {
+    /**
+     * Creates post from the database
+     * 
+     * @param postID
+     */
+    public Post(long postID) {
     	this.postID = postID;
-    	// TODO: get text and title from db.
+
+    	String q_getPost = "SELECT * FROM `posts` " + 
+    	                   " WHERE `postid` = ?";
+    	String[] p_getPost = {String.valueOf(postID)};    	
+    	
+    	HashMap<String, String> row = (new DB()).query(q_getPost, p_getPost).get(0);
+    	this.title = row.get("title");
+    	this.text = row.get("text");
+    	this.classroomID = Integer.parseInt(row.get("classroomid"));
     }
     
-    public void save() {
-    	// TODO: save post in db and set the postid
+    /**
+     * Saves the post to the db.
+     */
+    public long save() {
+       String setFields = "`title` = '?', `text` = ?, `classroomid` = ?";
+       String classroomIDStr = String.valueOf(this.classroomID); 
+       String[] p_savePost = {
+          this.title,
+          this.text,
+          classroomIDStr,
+          this.title,
+          this.text,
+          classroomIDStr,
+          String.valueOf(this.postID) 
+       };
+
+       String q_savePost = "INSERT INTO `posts` SET " + setFields +
+                           " ON DUPLICATE KEY UPDATE " + setFields +
+                           " WHERE `postid` = ?";
+       (new DB()).query(q_savePost, p_savePost);
+       
+       // This may not be set here...
+       // TODO: return latest updated index from the db and set it here
+       return this.postID;
     }
 }
 
--- test/TestPost.java
+++ test/TestPost.java
@@ -9,12 +9,45 @@ import logic.Post;
 
 public class TestPost {
 	@Test
-	public void testPostConstructor() {
+	public void testPostNewConstructor() {
 		String title = "Test Post";
 		String text = "This is a post created by TestPostConstructor";
 		Post p = new Post(title, text);
 		assertEquals(title, p.title);
 		assertEquals(text, p.text);
+	}
+	
+	@Test
+	public void testPostConstructorDB() {
+		String title = "Please get me out of the db!";
+		String text = "All I want is to be with you!";
+	   Post op = new Post(title, text);
+	   
+	   // Save must be run before we can retrieve anything.
+	   long postID = op.save();
+	   
+	   Post post = new Post(postID);
+	   
+	   assertEquals(op.postID, post.postID);
+	   
+	}
+
+	@Test
+	public void testPostUpdate() {
+		String title = "Test Post";
+		String text = "All I want is to be with you!";
+	   Post op = new Post(title, text);
+
+	   // Save must be run before we can retrieve anything.
+	   long postID = op.save();
+
+		op.title = "Please get me out of the db!";
+	   op.save();
+	   
+	   Post post = new Post(postID);
+	   
+	   assertEquals(op.title, post.title);
+	   
 	}
 
 }
