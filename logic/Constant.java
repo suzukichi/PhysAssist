9b61f055b45e0cd1c241b0a112f2b9203202d57b
R3xKyle <kyle@ringler.com>
2017-03-11 21:40:03 -0800

    Task #644404 - Added scientific notation support to equation.

--- logic/Constant.java
+++ logic/Constant.java
@@ -1,25 +1,54 @@
 package logic;
 
 public class Constant implements Term {
-	private String name;
+	private String symbol;
+	private double value;
 
-	public Constant(String name)
+	public Constant(String symbol)
 	{
-		this.name = name;
+		this.symbol = symbol;
 	}
 
 	public double getValue()
 	{
 		//db lookup for constant name
-		return 0;
+		String valueString = "";
+		String[] p_getConstantValue = {
+		          DB.T_S, valueString,
+		       };
+
+		String q_getConstantValue = "SELECT `value`, FROM `constants` WHERE `symbol` = " + this.symbol;
+		(new DB()).query(q_getConstantValue, p_getConstantValue);
+		return normalizeValue(valueString);
 	}
 	
-	public String getName()
+	private double normalizeValue(String valueString) 
 	{
-		return name;
+		String[] halves = valueString.split("E");
+		if (halves.length == 2) {
+			this.value = (checkNegative(halves[0])) * Math.pow(10, checkNegative(halves[1]));
+		}
+		else {
+			this.value = checkNegative(halves[0]);
+		}
+		return value;
+	}
+	
+	private double checkNegative(String string) {
+		if ('-' == string.charAt(0))
+		{
+			return (0 - Double.parseDouble(string.substring(1, string.length())));
+		}
+		else {
+			return Double.parseDouble(string);
+		}
+	}
+
+	public String getName() {
+		return symbol;
 	}
 
 	public String toString() {
-		return name;
+		return symbol;
 	}
 }
