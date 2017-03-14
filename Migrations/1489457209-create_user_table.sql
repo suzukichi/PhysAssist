159461439d9200a5134954cfc1d7183f5e0f958f
Diana Chiu <suzukichi@users.noreply.github.com>
2017-03-14 09:45:48 -0700

    Add test data for classrooms and students

--- Migrations/1489457209-create_user_table.sql
+++ Migrations/1489457209-create_user_table.sql
@@ -9,3 +9,12 @@ CREATE TABLE `users` (
    PRIMARY KEY `userid` (`userid`),
    INDEX `username` (`username`)
 );
+
+-- TEST DATA
+INSERT INTO `users` (`username`, `password`, `first_name`, `last_name`,
+ `email`, `registration_date`) 
+VALUES
+("bob", "123", "Bob", "Smith", "bob@smith.com", UNIX_TIMESTAMP(NOW())),
+("prof", "bla", "Professor", "Watt", "prof@something.edu", UNIX_TIMESTAMP(NOW())),
+("sam", "argh", "sam", "wise", "sam@theshire.me", UNIX_TIMESTAMP(NOW())),
+("slacker", "noschool123", "mister", "truant", "slacker@", UNIX_TIMESTAMP(NOW()))
--- Migrations/1489457486-create_class_tables.sql
+++ Migrations/1489457486-create_class_tables.sql
@@ -10,8 +10,24 @@ CREATE TABLE `classrooms` (
    INDEX `status` (`status`)
 );
 
+-- FILL CLASSROOMS
+INSERT INTO `classrooms` (`title`, `description`, `ownerID`, `start_date`, `status`) 
+VALUES 
+("Physics1", "A physics class.", 1, UNIX_TIMESTAMP(NOW()), 0),
+("Physics2", "Another physics class.", 1, UNIX_TIMESTAMP(NOW()), 0),
+("Physics3", "A third physics class.", 1, UNIX_TIMESTAMP(NOW()), 1);
+
+-- STUDENT TABLES
 CREATE TABLE `students` (
    `userid` INT(11),
    `classroomid` INT(11),
    PRIMARY KEY `userid` (`userid`, `classroomid`)
 );
+
+-- FILL STUDENTS
+INSERT INTO `students` (`userid`, `classroomid`)
+VALUES 
+(1, 1),
+(2, 1),
+(2, 2);
+
