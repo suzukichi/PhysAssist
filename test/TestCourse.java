6b9fc75a3344b73d9e6b85696a6f1ee31eb38a16
Daniel Sabsay <dsabsay@calpoly.edu>
2017-03-16 11:24:46 -0700

    #645566 - Integration testing, TestCourseWithPosts is failing

--- test/TestCourse.java
+++ test/TestCourse.java
@@ -4,6 +4,7 @@ package test;
  * TestCourse.java
  * 
  * Tests for the Course class.
+ * Integration Tests: Course.java, Post.java, Database.java
  * 
  * @author Daniel Sabsay
  * Created March 14, 2017
@@ -18,6 +19,9 @@ import logic.Post;
 
 public class TestCourse {
 
+  /*
+   * Integration Test: Course.java, DB.java
+   */
   @Test
   public void TestCourseConstructionDB() {
     Course course = new Course(50, "test course", "Description of test course.", 1);
@@ -25,17 +29,22 @@ public class TestCourse {
     
     Course fromDB = new Course(course.courseID);
     //TODO should compare objects
-    assertEquals(course.courseName, fromDB.courseName);
+    //assertEquals(course.courseName, fromDB.courseName);
+    assertEquals(course, fromDB);
     
     course.delete();
   }
 
+  /*
+   * Integration Test: Course.java, Post.java, DB.java
+   */
   @Test
   public void TestCourseWithPosts() {
     Course course = new Course(50, "test course", "Description of test course.", 1);
     course.save();
 
     Post p = new Post("test post", "This is a test post for a test course", 100);
+    //TODO maybe right an addPost() method that would also save the post to the DB.
     course.posts.add(p);
     
     // save to DB
