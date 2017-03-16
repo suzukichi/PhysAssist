ddcbb437111db66824d08516ed629f829499e41f
suzukichi <dmchiu@calpoly.edu>
2017-03-16 16:23:54 -0700

    Fix getEquation query
    Also cleans up style of code.

--- logic/Equation.java
+++ logic/Equation.java
@@ -5,91 +5,74 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Equation {
-	private String name;
-	private int id;
-	public long topicID;
-	private String view;
-	private String description;
-	private String history;
+   private String name;
+   private int id;
+   public long topicID;
+   private String view;
+   private String description;
+   private String history;
 
-	public Equation(String name)
-	{
-		this.name = name;
-		getEquation();
-	}
+   public Equation(String name) {
+      this.name = name;
+      getEquation();
+   }
 
-	private void getEquation()
-	{
-		String[] pgetEquation = {
-		          DB.T_S, name
-		       };
+   private void getEquation() {
+      String[] pGetEquation = {DB.T_S, this.name};
 
-		String qgetEquation = "SELECT `id`, `view`, `description`, `history`, FROM `equations` WHERE `name` = ?";
-		ArrayList<HashMap<String, String>> rows = (DB.getInstance()).query(qgetEquation, pgetEquation);
-		
-		for (HashMap<String, String> row : rows) 
-		{
-			this.id = Integer.parseInt(row.get("id"));
-			this.view = row.get("view");
-			this.description = row.get("description");
-			this.history = row.get("history");
-			this.topicID = Long.parseLong(row.get("topicid"));
-		}
-	}
+      String qGetEquation = "SELECT `equationid`, `view`, `description`, `history` FROM `equations` WHERE `name` = ?";
+      ArrayList<HashMap<String, String>> rows = DB.getInstance().query(qGetEquation, pGetEquation);
 
-	public String getView()
-	{
-		return view;
-	}
+      for (HashMap<String, String> row : rows) {
+         this.id = Integer.parseInt(row.get("equationid"));
+         this.view = row.get("view");
+         this.description = row.get("description");
+         this.history = row.get("history");
+         this.topicID = Long.parseLong(row.get("topicid"));
+      }
+   }
 
-	public String getDescription()
-	{
-		return description;
-	}
+   public String getView() {
+      return view;
+   }
 
-	public String getHistory()
-	{
-		return history;
-	}
+   public String getDescription() {
+      return description;
+   }
 
-	public Term solve(Term target, List<Term> terms) throws Exception
-	{
-		String ast = getAST(target);
-		for (int i = 0; i < terms.size(); i++)
-		{
-			String[] halves = ast.split((terms.get(i)).getName());
-			if (halves.length == 2)
-			{
-				ast = halves[0] + (terms.get(i)).getValue() + halves[1];
-			}
-			else
-			{
-				throw new IllegalArgumentException("Invalid ast string.");
-			}
-		}
-		AST equation = new AST(ast);
-		return equation.compute();
-	}
+   public String getHistory() {
+      return history;
+   }
 
-	private String getAST(Term term)
-	{
-		String ast = "";
-		String[] pGetAST = {
-				  DB.T_I, String.valueOf(id),
-		          DB.T_S, term.getName()
-		       };
+   public Term solve(Term target, List<Term> terms) throws Exception {
+      String ast = getAST(target);
+      for (int i = 0; i < terms.size(); i++) {
+         String[] halves = ast.split((terms.get(i)).getName());
+         if (halves.length == 2) {
+            ast = halves[0] + (terms.get(i)).getValue() + halves[1];
+         } else {
+            throw new IllegalArgumentException("Invalid ast string.");
+         }
+      }
+      AST equation = new AST(ast);
+      return equation.compute();
+   }
 
-		String qGetAST = "SELECT `ast` FROM `ast` WHERE `eqid` = ?, `term` = ?";
-		ArrayList<HashMap<String, String>> rows = (DB.getInstance()).query(qGetAST, pGetAST);
-		
-		for (HashMap<String, String> row : rows) 
-		{
-			ast = row.get("view");
-		}
-		if ("".equals(ast))
-		{
-			throw new IllegalArgumentException("Invalid ast string.");
-		}
-        return ast;
-	}
+   private String getAST(Term term) {
+      String ast = "";
+      String[] pGetAST = { DB.T_I, String.valueOf(id), DB.T_S, term.getName() };
+
+      String qGetAST = "SELECT `ast` FROM `ast` WHERE `eqid` = ?, `term` = ?";
+      ArrayList<HashMap<String, String>> rows = DB.getInstance().query(qGetAST, pGetAST);
+
+      for (HashMap<String, String> row : rows) {
+         ast = row.get("view");
+      }
+
+      if ("".equals(ast)) {
+         throw new IllegalArgumentException("Invalid ast string.");
+      }
+
+      return ast;
+   }
 }
--- test/TestEquation.java
+++ test/TestEquation.java
@@ -1,9 +1,11 @@
 package test;
+
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 
 import org.junit.Test;
+
 import logic.Equation;
 import logic.Term;
 import logic.Variable;
@@ -17,49 +19,50 @@ import logic.Variable;
  * @author Arianna Olson
  */
 public class TestEquation {
-	private Equation equation;
+   private Equation equation;
 
-	/*
-	 * Integration test: Equation.java, DB.java, Variable.java, Term.java, AST.java
-	 */
-	@Test public void TestEquationSolve() throws Exception
-	{
-		String equationName = "Newton's Second Law of Motion";
-		Term solveFor = new Variable("F", 0.0);
-		ArrayList<Term> terms = new ArrayList<Term>();
-		terms.add(new Variable("m", 9.0));
-		terms.add(new Variable("a", 5.0));
-		equation = new Equation(equationName);
-		assertEquals(42.0, (equation.solve(solveFor, terms)).getValue(), 0.01);
-	}
-	
-	/*
-	 * Integration test: Equation.java, DB.java
-	 */
-	@Test public void TestEquationGetView()
-	{
-		String equationName = "Newton's Second Law of Motion";
-		String expectedView = "F = m * a";
-		
-		Equation eq = new Equation(equationName);
-		assertEquals(expectedView, eq.getView());
-	}
-	
-	@Test public void TestEquationGetDescription()
-	{
-		String equationName = "Newton's Second Law of Motion";
-		String expectedDescription = "The force (F) acting on an object is equal to the mass (m) of an object times its acceleration (a).";
-		
-		Equation eq = new Equation(equationName);
-		assertEquals(expectedDescription, eq.getDescription());
-	}
-	
-	@Test public void TestEquationGetHistory()
-	{
-		String equationName = "Newton's Second Law of Motion";
-		String expectedHistory = "Isaac Newton's laws of motion were first set down in his Principia Mathematica Philosophiae Naturalis in 1687.";
-		
-		Equation eq = new Equation(equationName);
-		assertEquals(expectedHistory, eq.getHistory());
-	}
+   /*
+    * Integration test: Equation.java, DB.java, Variable.java, Term.java,
+    * AST.java
+    */
+   @Test
+   public void TestEquationSolve() throws Exception {
+      String equationName = "Newton's Second Law of Motion";
+      Term solveFor = new Variable("F", 0.0);
+      ArrayList<Term> terms = new ArrayList<Term>();
+      terms.add(new Variable("m", 9.0));
+      terms.add(new Variable("a", 5.0));
+      equation = new Equation(equationName);
+      assertEquals(42.0, (equation.solve(solveFor, terms)).getValue(), 0.01);
+   }
+
+   /*
+    * Integration test: Equation.java, DB.java
+    */
+   @Test
+   public void TestEquationGetView() {
+      String equationName = "Newton's Second Law of Motion";
+      String expectedView = "F = m * a";
+
+      Equation eq = new Equation(equationName);
+      assertEquals(expectedView, eq.getView());
+   }
+
+   @Test
+   public void TestEquationGetDescription() {
+      String equationName = "Newton's Second Law of Motion";
+      String expectedDescription = "The force (F) acting on an object is equal to the mass (m) of an object times its acceleration (a).";
+
+      Equation eq = new Equation(equationName);
+      assertEquals(expectedDescription, eq.getDescription());
+   }
+
+   @Test
+   public void TestEquationGetHistory() {
+      String equationName = "Newton's Second Law of Motion";
+      String expectedHistory = "Isaac Newton's laws of motion were first set down in his Principia Mathematica Philosophiae Naturalis in 1687.";
+
+      Equation eq = new Equation(equationName);
+      assertEquals(expectedHistory, eq.getHistory());
+   }
 }
