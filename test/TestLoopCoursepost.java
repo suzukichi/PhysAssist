e147b09e63fdfb5973cf26e76c66df8ba7530d42
ssulkar <ssulkar@calpoly.edu>
2017-03-16 19:16:58 -0700

    loop test for course post

---
+++ test/TestLoopCoursepost.java
@@ -0,0 +1,40 @@
+package test;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import logic.Post;
+import logic.Course;
+public class TestLoopCoursepost {
+	
+	@Test
+	public void LoopNever(){
+		Course c = new Course(1,"SE", "Programming");
+		String never = c.toString();
+		assertEquals("{Course:  courseID: 0 courseName: SE description: Programming posts: [\n"+
+				"   ]}\n",never);
+	}
+	
+	@Test
+	public void LoopOnce(){
+		Course c1 = new Course(1,"SE", "Programming");
+		Post p1 = new Post("abc","def");
+		c1.addPost(p1);
+		String one = c1.toString();
+		assertEquals("{Course:  courseID: 0 courseName: SE description: Programming posts: [\n"
+				+"      Post: abc, def, \n"
+				+"   ]}\n",one);
+	}
+	
+	@Test
+	public void LoopN(){
+		Course c2 = new Course(1,"SE", "Programming");
+		Post p3 = new Post("abc","def");
+		Post p4 = new Post("def","efg");
+		c2.addPost(p3);
+		c2.addPost(p4);
+		String N = c2.toString();
+		assertEquals("{Course:  courseID: 0 courseName: SE description: Programming posts: [\n"
+				+"      Post: abc, def, \n"
+				+"      Post: def, efg, \n"
+				+"   ]}\n",N);
+	}
+}
