45dedd68ee015b1f3a8f15e28e77a15d2bd84d86
ssulkar <ssulkar@calpoly.edu>
2017-03-16 19:28:38 -0700

    upper case stuff again

--- test/TestLoopCalculator.java
+++ test/TestLoopCalculator.java
@@ -6,25 +6,25 @@ import static org.junit.Assert.*;
 import logic.Calculator;
 public class TestLoopCalculator {
 	@Test
-	public void LoopNever(){
+	public void loopNever(){
 		Calculator C = new Calculator();
 		int i = C.initButtons(0);
 		assertEquals(0,i,0);
 	}
 	@Test
-	public void LoopOnce(){
+	public void loopOnce(){
 		Calculator C = new Calculator();
 		int i = C.initButtons(1);
 		assertEquals(1,i,0);
 	}
 	@Test
-	public void LoopNhalf(){
+	public void loopNhalf(){
 		Calculator C = new Calculator();
 		int i = C.initButtons(15);
 		assertEquals(15,i,0);
 	}
 	@Test
-	public void LoopNTimes(){
+	public void loopNTimes(){
 		Calculator C = new Calculator();
 		int i = C.initButtons(30);
 		assertEquals(30,i,0);
